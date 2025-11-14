674cb7a72c752432b59cbb23a5fe49f64869d03c
{Happy <happyswaraj7667@gmail.com>
2025-11-14 20:49:35 +0530

    basic

---
+++ basic/first/02_multiline.py
@@ -0,0 +1,8 @@
+print(''' Python is a high-level, general-purpose programming language that is easy to read, easy to write, and widely used for web development, data science, machine learning, automation, and many other applications.
+      
+Why it’s popular:
+
+Simple English-like syntax
+Huge libraries (NumPy, Pandas, Django, etc.)
+Works everywhere (Windows, Mac, Linux)
+Easy for beginners, powerful for experts ''')
---
+++ basic/first/03_comments.py
@@ -0,0 +1,23 @@
+# about myself (single line comment)
+
+
+print("hii this is happy swaraj")
+
+
+
+#  multi-line comments in two ways
+
+
+"""
+happy swaraj
+12205899
+9128730395
+dev_happy02
+"""
+
+'''
+happy swaraj
+12205899
+9128730395
+dev_happy02
+'''
