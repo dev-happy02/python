5f34882ad2e0a010b589653fe1b751cc174c784c
{Happy <happyswaraj7667@gmail.com>
2025-11-14 22:21:05 +0530

    update

---
+++ basic/first/05_datatype.py
@@ -0,0 +1,26 @@
+a = 1        # a is an integer
+
+b = 5.22     # b is a floating point number
+
+c = "Happy"  # c is a string
+
+d = False    # d is a boolean variable
+
+e = None     # e is a none type variable
+
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+
+
+# Ruels..
+
+# Variable cannot start with a number.
+# Can contain letters, digits, and underscore (_) only.
+# No spaces allowed.
+# Case-sensitive (age ≠ Age).
+# Keywords cannot be used as variable names.
+# No special characters allowed (like @, #, -, $).
+# Name should be meaningful.
