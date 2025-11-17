9e8aa0ec8996c9f9f145cc6c7d3a2238ca621133
{Happy <happyswaraj7667@gmail.com>
2025-11-17 20:29:49 +0530

    list and tuples

---
+++ basic/04 list and tuples/03_tuple.py
@@ -0,0 +1,9 @@
+a= (12,11,54)
+b= (1,) # for printing single tuple
+c= ("happy", 324, 64.54, True)
+
+print(a)
+print(b)
+print(c)
+print(type(c))
+print(type(a))
---
+++ basic/04 list and tuples/04_tuple_method.py
@@ -0,0 +1,9 @@
+numbers = (10, 20, 30, 20, 40, 60)
+
+print("Tuple:", numbers)
+
+# 1. count() – count occurrences of a value
+print("1. count(20):", numbers.count(20))
+
+# 2. index() – find first index of a value
+print("2. index(30):", numbers.index(30))
